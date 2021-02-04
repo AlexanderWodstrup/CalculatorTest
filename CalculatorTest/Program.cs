c7802cf4ecbfd229125ec92a62d8c1e331689277
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-02-04 14:22:53 +0100

    New test of add(5)

--- CalculatorTest/Program.cs
+++ CalculatorTest/Program.cs
@@ -11,6 +11,8 @@ namespace CalculatorTest
 
             Console.WriteLine($"Test af Add funktion, 5 + 5 = {uut.Add(5,5)}");
 
+            Console.WriteLine($"Test af Add funktion, tidligere resultat + 5 = {uut.Add(5)}");
+
             Console.WriteLine($"Test af Subtract funktion, 7 - 3 = {uut.Subtract(7,3)}");
 
             Console.WriteLine($"Test af Multiply funktion, 6 * 6 = {uut.Multiply(6,6)}");
